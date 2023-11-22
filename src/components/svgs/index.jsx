7304cb56f5368aed64673ff840b136365d451510
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-11-22 12:46:27 +0100

    feat:auth page

---
+++ src/components/svgs/index.jsx
@@ -0,0 +1,4 @@
+export { default as LogoIcon } from './LogoSvg'
+export { default as WhatsAppIcon } from './WhatAppSvg'
+export { default as AppleIcon } from './AppleSvg'
+export { default as GoogleIcon } from './GoogleSvg'
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom'
-
-Outlet
+import { LogoIcon } from '../components/svgs'
 
 const AuthLayout = () => {
   return (
-    <div>
-      <Outlet />
+    <div className='max-w-[1440px] mx-auto relative'>
+      <div className='items-center hidden md:flex pl-10 pt-10 sticky top-0'>
+        <LogoIcon />
+        <p className=' text-[#027600] font-bold font-orbitron'>TrackMed</p>
+      </div>
+      <div className='mx-auto max-w-[600px]'>
+        <Outlet />
+      </div>
     </div>
   )
 }
