3b3570e089ac5c0f7cdba14fb9766d941eaa8940
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-11-25 19:16:43 +0100

    page preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import { AppProvider } from './contexts/AppContext'
-import UserProvider  from './contexts/UserContext'
+import UserProvider from './contexts/UserContext'
+import { useEffect, useState } from 'react'
+import { PreLoader } from './components'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const isLoaded = sessionStorage.getItem('isLoaded')
+    if (isLoaded) {
+      setIsLoading(false)
+    } else {
+      setTimeout(() => {
+        setIsLoading(false)
+        sessionStorage.setItem('isLoaded', 'true')
+      }, 2000)
+    }
+  }, [])
+
   return (
     <UserProvider>
       <AppProvider>
-        <RouterProvider router={router} />
+        {isLoading ? <PreLoader /> : <RouterProvider router={router} />}
       </AppProvider>
     </UserProvider>
   )
